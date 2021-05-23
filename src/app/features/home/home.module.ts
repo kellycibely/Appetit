63f0c85b1b232a6f25baa3b7e9a1d76c5164e5bf
kelly cibely <kellycibely@gmail.com>
2021-05-23 17:54:16 -0300

    finalizado titule home page

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { MyCommonModule } from '../../ui/myCommon.module';
 
 export const routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [HomeComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), MyCommonModule],
 })
 export class HomeModule {}
