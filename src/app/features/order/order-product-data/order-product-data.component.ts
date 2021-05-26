756d5ae64e84de512bcb45263672b44c564e3812
kelly cibely <kellycibely@gmail.com>
2021-05-26 19:20:16 -0300

    chore: removido codigo nao utilizado

--- src/app/features/order/order-product-data/order-product-data.component.ts
+++ src/app/features/order/order-product-data/order-product-data.component.ts
@@ -4,10 +4,9 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-order-product-data',
   templateUrl: './order-product-data.component.html',
-  styleUrls: ['./order-product-data.component.css']
+  styleUrls: ['./order-product-data.component.css'],
 })
 export class OrderProductDataComponent implements OnInit {
-
   @Input() productSelected: any;
   @Output() productObservation = new EventEmitter();
 
@@ -17,24 +16,23 @@ export class OrderProductDataComponent implements OnInit {
   totalOrder: number;
   total: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.totalOrder = this.quantity * this.productSelected.value;
     this.total = this.totalOrder;
-    console.log(this.productSelected);
   }
 
   remove() {
     if (this.quantity != 1) {
       this.quantity--;
-      this. calculeTotal(); 
+      this.calculeTotal();
     }
   }
 
-  add () {
+  add() {
     this.quantity++;
-    this. calculeTotal();
+    this.calculeTotal();
   }
 
   calculeTotal() {
@@ -48,12 +46,11 @@ export class OrderProductDataComponent implements OnInit {
       image: this.productSelected.image,
       description: this.productSelected.name,
       quantity: this.quantity,
-      total : this.total,
+      total: this.total,
       pasta: this.activePasta,
-      observation: this.observation
-    }
+      observation: this.observation,
+    };
 
     this.productObservation.emit(select);
   }
-
 }
