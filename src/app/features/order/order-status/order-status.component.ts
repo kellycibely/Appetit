8c566af25468aab27fc9c50a0c5e164c743983d5
kelly cibely <kelly@tartigrado.com>
2021-05-26 15:24:44 -0300

    correçao navbar

--- src/app/features/order/order-status/order-status.component.ts
+++ src/app/features/order/order-status/order-status.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import {MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-status',
   templateUrl: './order-status.component.html',
-  styleUrls: ['./order-status.component.css']
+  styleUrls: ['./order-status.component.css'],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-br'},
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+
+  ],
 })
 export class OrderStatusComponent implements OnInit {
 
